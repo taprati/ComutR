3c76e0e446afd695045df4935cab3336594043fa
taprati <tyleraprati@gmail.com>
2024-02-02 15:23:20 -0500

    Remove absolute sizing of comut body

--- example/testing_comut.R
+++ example/testing_comut.R
@@ -18,7 +18,18 @@ input_maf <- tribble(
   "2", "C", "In_Frame_Del",
   #"3", "A", "Missense_Mutation",
   "4", "A", "Nonsense_Mutation",
-  "4", "B", "Nonsense_Mutation"
+  "4", "B", "Nonsense_Mutation",
+  "1", "D", "Missense_Mutation",
+  "1", "E", "Nonsense_Mutation",
+  "1", "F", "In_Frame_Del",
+  "2", "G", "In_Frame_Del",
+  #"3", "A", "Missense_Mutation",
+  "4", "G", "Nonsense_Mutation",
+  "4", "H", "Nonsense_Mutation",
+  "5", "A", "Nonsense_Mutation",
+  "5", "B", "Nonsense_Mutation",
+  "6", "D", "Missense_Mutation",
+  "6", "E", "Nonsense_Mutation",
   )
   meta <- tribble(
   ~Tumor_Sample_Barcode, ~Gender, ~Biopsy,
@@ -26,22 +37,28 @@ input_maf <- tribble(
   "2", "Female", "Metastasis",
   "3", "Male", "Metastasis",
   #"4", "Female", "Primary"
+  "5", "Female", "Metastasis",
+  "6", "Male", "Primary",
 )
 signatures <- tribble(
   ~Tumor_Sample_Barcode, ~Aging, ~Other,
   "1", 0.5, 0.5,
   "2", 0.25, 0.75,
   "3", 0.75, 0.25,
-  "4", 1, 0
+  "4", 1, 0,
+  "5", 0.25, 0.75,
+  "6", 0.75, 0.25,
 )
 total_snp_data <- tribble(
   ~Tumor_Sample_Barcode, ~n_snps,
   "1", 10,
   "2", 4,
   "3", 2,
-  "4", 7
+  "4", 7,
+  "5", 5,
+  "6", 8,
 )
-ids <- as.character(c(1,2,3,4))
+ids <- as.character(1:6)
 
 ### Color scheme creation
 signature_colors <- c(
@@ -80,8 +97,8 @@ comut_plot <- comut(
   data = input_maf,
   metadata = meta,
   col_maps = color_maps,
-  id_order = c("1", "2", "3", "4"),
-  grob = TRUE,
+  id_order = as.character(1:6),
+  # grob = TRUE,
   barplot_data = barplot_data
 )
 comut_plot
